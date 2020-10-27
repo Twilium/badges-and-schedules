30482b17a4cac55e5c68d13dcef46feb763dbb8a
Emmanuel <rivera128@live.com>
2020-10-27 01:12:04 -0500

    commit tree 40fa07f9f09a03f61d8d58eec9b338a52b112609

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    new_array = []
+    names.each do |name|
+        new_array.push("Hello, my name is #{name}.")
+    end
+    new_array
+end
+
+def assign_rooms(speakers)
+    room_number = 1
+    room_assignments = []
+    speakers.each do |speaker|
+        room_assignments.push("Hello, #{speaker}! You'll be assigned to room #{room_number}!")
+        room_number +=1
+    end
+    room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each{|attendee| puts attendee}
+    assign_rooms(attendees).each{|rooms| puts rooms}
+end
